47d8bd7b0dbcfed198e419417d2e01e940d6829e
Yash Harjani <yashharjani2001@gmail.com>
2025-06-17 17:52:46 -0300

    first commit

--- src/cognito/config.js
+++ src/cognito/config.js
@@ -1,5 +1,5 @@
 export const COGNITO_CONFIG = {
   userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
-  clientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
+  clientId: import.meta.env.VITE_COGNITO_USER_POOL_CLIENT,
   region: import.meta.env.VITE_AWS_REGION
 };
