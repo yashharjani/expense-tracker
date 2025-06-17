0b90a9d74653235177fa322afce0b3b723f0cb1e
Yash Harjani <yashharjani2001@gmail.com>
2025-06-17 12:38:26 -0300

    first commit

--- src/cognito/config.js
+++ src/cognito/config.js
@@ -1,5 +1,5 @@
 export const COGNITO_CONFIG = {
-  userPoolId: "us-east-1_m5IchDTP2",
-  clientId: "72nvdqua2u6i3e5o6faontif6d",
-  region: "us-east-1"
+  userPoolId: import.meta.env.VITE_COGNITO_USER_POOL_ID,
+  clientId: import.meta.env.VITE_COGNITO_CLIENT_ID,
+  region: import.meta.env.VITE_AWS_REGION
 };
--- src/config.js
+++ src/config.js
@@ -1,1 +1,2 @@
-export const API_BASE_URL = "https://pstxt853t1.execute-api.us-east-1.amazonaws.com";
+// export const API_BASE_URL = "https://pstxt853t1.execute-api.us-east-1.amazonaws.com";
+export const API_BASE_URL = import.meta.env.VITE_API_URL;
